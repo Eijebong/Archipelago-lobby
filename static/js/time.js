beba2e85a3fa2f3473247feb902032cc59a81c7b
Bastien Orivel <eijebong@bananium.fr>
2024-02-24 22:31:32 +0100

    Refactor how time is transformed to be able to use it on every page

---
+++ static/js/time.js
@@ -0,0 +1,20 @@
+const timeElements = document.getElementsByClassName("time");
+
+for (const timeEl of timeElements) {
+    const isLongTime = timeEl.classList.contains("long-time");
+    var format;
+    if (isLongTime) {
+        format = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', second: '2-digit', timeZoneName: 'longGeneric'}
+    }
+
+    if (!format) {
+        console.log("Time element missing proper time format, ignoring");
+        continue;
+    }
+
+    const time = timeEl.innerText + "Z";
+    const parsedTime = Date.parse(time);
+    timeEl.innerText = new Intl.DateTimeFormat('default',
+        format
+    ).format(parsedTime);
+}
