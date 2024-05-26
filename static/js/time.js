240ce9f5dabd2e38773e0431a38f2aae01ee66b4
Bastien Orivel <eijebong@bananium.fr>
2024-05-26 13:10:31 +0200

    Add a way to copy timestamps from rooms as discord time.
    Fixes #2

--- static/js/time.js
+++ static/js/time.js
@@ -2,6 +2,7 @@ const timeElements = document.getElementsByClassName("time");
 
 for (const timeEl of timeElements) {
     const isLongTime = timeEl.classList.contains("long-time");
+    const enableDiscordCopy = timeEl.classList.contains("discord");
     var format;
     if (isLongTime) {
         format = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', timeZoneName: 'longGeneric'}
@@ -17,4 +18,17 @@ for (const timeEl of timeElements) {
     timeEl.innerText = new Intl.DateTimeFormat('default',
         format
     ).format(parsedTime);
+
+    if (enableDiscordCopy) {
+        const copyEl = document.createElement("i");
+        copyEl.title = "Copy discord timestamp";
+        copyEl.onclick = () => {
+            navigator.clipboard.writeText("<t:" + parsedTime / 1000 + ":F>").await;
+            copyEl.classList = "fa fa-check copy-button"
+            setTimeout(() => copyEl.classList = "fa-regular fa-copy copy-button", 500)
+            return true;
+        }
+        copyEl.classList = "fa-regular fa-copy copy-button";
+        timeEl.insertAdjacentElement('beforeend', copyEl);
+    }
 }
