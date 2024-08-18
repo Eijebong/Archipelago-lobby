def84164badd1c295ffba4b10a70d5ca26779104
Bastien Orivel <eijebong@bananium.fr>
2024-08-18 16:10:34 +0200

    Display span titles on parent hover

---
+++ static/js/span_title.js
@@ -0,0 +1,29 @@
+const spanElements = document.getElementsByTagName("span");
+
+for (const span of spanElements) {
+    const hasTitle = span.title !== undefined && span.title !== "";
+
+    if (!hasTitle) {
+        continue;
+    }
+
+    const parent = span.parentElement;
+    const titleElem = document.createElement("span");
+    titleElem.innerHTML = " (" + span.title + ")";
+    titleElem.style.display = 'none';
+    titleElem.classList = "span-title"
+    if (titleElem.nextSibling) {
+        parent.insertBefore(titleElem, titleElem.nextSibling);
+    } else {
+        parent.appendChild(titleElem);
+    }
+
+    parent.addEventListener('mouseover', () => {
+        titleElem.style.display = 'inline';
+    });
+    parent.addEventListener('mouseleave', () => {
+        titleElem.style.display = 'none';
+    });
+
+}
+
