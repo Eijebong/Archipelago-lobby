0b71da7bc08bf6bde01b5fbec66794c066d68353
Bastien Orivel <eijebong@bananium.fr>
2024-09-10 19:17:31 +0200

    Create title spans as needed instead of creating them on page load
    With 1800+ YAMLs this starts mattering a **lot**.

--- static/js/span_title.js
+++ static/js/span_title.js
@@ -1,4 +1,5 @@
 const spanElements = document.getElementsByTagName("span");
+var hasTitleShown = false;
 
 for (const span of spanElements) {
     const hasTitle = span.title !== undefined && span.title !== "";
@@ -8,22 +9,29 @@ for (const span of spanElements) {
     }
 
     const parent = span.parentElement;
-    const titleElem = document.createElement("span");
-    titleElem.innerHTML = " (" + span.title + ")";
-    titleElem.style.display = 'none';
-  titleElem.classList = "span-title"
-    if (span.nextSibling) {
-        parent.insertBefore(titleElem, span.nextSibling);
-    } else {
-        parent.appendChild(titleElem);
-    }
 
     parent.addEventListener('mouseover', () => {
+        if (hasTitleShown) {
+            return
+        }
+
+        hasTitleShown = true;
+        const titleElem = document.createElement("span");
+        titleElem.innerHTML = " (" + span.title + ")";
+        titleElem.classList = "span-title"
         titleElem.style.display = 'inline';
+        if (span.nextSibling) {
+            parent.insertBefore(titleElem, span.nextSibling);
+        } else {
+            parent.appendChild(titleElem);
+        }
+
+        parent.addEventListener('mouseleave', () => {
+            hasTitleShown = false;
+            parent.removeChild(titleElem);
+        }, {once: true});
     });
-    parent.addEventListener('mouseleave', () => {
-        titleElem.style.display = 'none';
-    });
+
 
 }
 
