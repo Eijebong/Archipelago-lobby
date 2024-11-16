e12d8b01ea2d8043613f6261bdc295b8cac8a987
Bastien Orivel <eijebong@bananium.fr>
2024-11-16 11:45:00 +0100

    Move room db related things into their own module

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -4,9 +4,6 @@ use crate::error::Result;
 use crate::extractor::YamlFeatures;
 use crate::schema::{discord_users, rooms, yamls};
 
-use anyhow::Context;
-use apwm::{Index, Manifest};
-use chrono::{NaiveDateTime, Timelike};
 use diesel::dsl::{exists, now, AsSelect, SqlTypeOf};
 use diesel::pg::Pg;
 use diesel::prelude::*;
@@ -15,27 +12,10 @@ use uuid::Uuid;
 
 pub mod instrumentation;
 mod json;
+mod room;
 
 pub use json::Json;
-
-#[derive(Insertable, diesel::AsChangeset, Debug)]
-#[diesel(table_name=rooms)]
-pub struct NewRoom<'a> {
-    pub id: Uuid,
-    pub name: &'a str,
-    pub close_date: NaiveDateTime,
-    pub description: &'a str,
-    pub room_url: &'a str,
-    pub author_id: Option<i64>,
-    #[diesel(treat_none_as_null = true)]
-    pub yaml_limit_per_user: Option<i32>,
-    pub private: bool,
-    pub yaml_validation: bool,
-    pub allow_unsupported: bool,
-    pub yaml_limit_bypass_list: Vec<i64>,
-    pub manifest: Json<Manifest>,
-    pub show_apworlds: bool,
-}
+pub use room::*;
 
 #[derive(Insertable)]
 #[diesel(table_name=yamls)]
@@ -47,60 +27,6 @@ pub struct NewYaml<'a> {
     player_name: &'a str,
     game: &'a str,
     features: json::Json<YamlFeatures>,
-}
-
-#[derive(Debug, Clone, diesel::Queryable, diesel::Selectable)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct Room {
-    pub id: Uuid,
-    #[diesel(embed)]
-    pub settings: RoomSettings,
-}
-
-#[derive(Debug, Clone, diesel::Queryable, diesel::Selectable)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-#[diesel(table_name=rooms)]
-pub struct RoomSettings {
-    pub name: String,
-    pub close_date: NaiveDateTime,
-    pub description: String,
-    pub room_url: String,
-    pub author_id: i64,
-    pub private: bool,
-    pub yaml_validation: bool,
-    pub allow_unsupported: bool,
-    pub yaml_limit_per_user: Option<i32>,
-    pub yaml_limit_bypass_list: Vec<i64>,
-    pub manifest: json::Json<Manifest>,
-    pub show_apworlds: bool,
-}
-
-impl RoomSettings {
-    pub fn default(index: &Index) -> Result<Self> {
-        Ok(Self {
-            name: "".to_string(),
-            close_date: chrono::Utc::now()
-                .naive_utc()
-                .with_second(0)
-                .context("Failed to create default datetime")?,
-            description: "".to_string(),
-            room_url: "".to_string(),
-            author_id: -1,
-            private: true,
-            yaml_validation: true,
-            allow_unsupported: false,
-            yaml_limit_per_user: None,
-            yaml_limit_bypass_list: vec![],
-            manifest: Json(Manifest::from_index_with_latest_versions(index)?),
-            show_apworlds: true,
-        })
-    }
-}
-
-impl Room {
-    pub fn is_closed(&self) -> bool {
-        self.settings.close_date < chrono::offset::Utc::now().naive_utc()
-    }
 }
 
 #[derive(Debug, diesel::Selectable, diesel::Queryable)]
@@ -165,42 +91,6 @@ pub async fn list_rooms(
 }
 
 #[tracing::instrument(skip(conn))]
-pub async fn create_room<'a>(
-    new_room: &'a NewRoom<'a>,
-    conn: &mut AsyncPgConnection,
-) -> Result<Room> {
-    Ok(diesel::insert_into(rooms::table)
-        .values(new_room)
-        .returning(Room::as_returning())
-        .get_result(conn)
-        .await?)
-}
-
-#[tracing::instrument(skip(conn))]
-pub async fn update_room<'a>(
-    new_room: &'a NewRoom<'a>,
-    conn: &mut AsyncPgConnection,
-) -> Result<()> {
-    diesel::update(rooms::table)
-        .filter(rooms::id.eq(new_room.id))
-        .set(new_room)
-        .execute(conn)
-        .await?;
-
-    Ok(())
-}
-
-#[tracing::instrument(skip(conn))]
-pub async fn delete_room(room_id: &uuid::Uuid, conn: &mut AsyncPgConnection) -> Result<()> {
-    diesel::delete(rooms::table)
-        .filter(rooms::id.eq(room_id))
-        .execute(conn)
-        .await?;
-
-    Ok(())
-}
-
-#[tracing::instrument(skip(conn))]
 pub async fn get_yamls_for_room_with_author_names(
     uuid: uuid::Uuid,
     conn: &mut AsyncPgConnection,
@@ -240,28 +130,6 @@ pub async fn get_yamls_for_room(
         .filter(yamls::room_id.eq(uuid))
         .select(Yaml::as_select())
         .get_results::<Yaml>(conn)
-        .await?)
-}
-
-#[tracing::instrument(skip(conn))]
-pub async fn get_room(uuid: uuid::Uuid, conn: &mut AsyncPgConnection) -> Result<Room> {
-    Ok(rooms::table
-        .find(uuid)
-        .select(Room::as_select())
-        .first::<Room>(conn)
-        .await?)
-}
-
-#[tracing::instrument(skip(conn))]
-pub async fn get_room_and_author(
-    uuid: uuid::Uuid,
-    conn: &mut AsyncPgConnection,
-) -> Result<(Room, String)> {
-    Ok(rooms::table
-        .find(uuid)
-        .inner_join(discord_users::table)
-        .select((Room::as_select(), discord_users::username))
-        .first(conn)
         .await?)
 }
 
---
+++ src/db/room.rs
@@ -0,0 +1,143 @@
+use anyhow::Context;
+use apwm::{Index, Manifest};
+use chrono::{NaiveDateTime, Timelike};
+use diesel::pg::Pg;
+use diesel::prelude::*;
+use diesel::{AsChangeset, Insertable, Queryable, Selectable};
+use diesel_async::{AsyncPgConnection, RunQueryDsl};
+use uuid::Uuid;
+
+use crate::db::Json;
+use crate::error::Result;
+use crate::schema::{discord_users, rooms};
+
+#[derive(Insertable, AsChangeset, Debug)]
+#[diesel(table_name=rooms)]
+pub struct NewRoom<'a> {
+    pub id: Uuid,
+    pub name: &'a str,
+    pub close_date: NaiveDateTime,
+    pub description: &'a str,
+    pub room_url: &'a str,
+    pub author_id: Option<i64>,
+    #[diesel(treat_none_as_null = true)]
+    pub yaml_limit_per_user: Option<i32>,
+    pub private: bool,
+    pub yaml_validation: bool,
+    pub allow_unsupported: bool,
+    pub yaml_limit_bypass_list: Vec<i64>,
+    pub manifest: Json<Manifest>,
+    pub show_apworlds: bool,
+}
+
+#[derive(Debug, Clone, Queryable, Selectable)]
+#[diesel(check_for_backend(Pg))]
+pub struct Room {
+    pub id: Uuid,
+    #[diesel(embed)]
+    pub settings: RoomSettings,
+}
+
+#[derive(Debug, Clone, Queryable, Selectable)]
+#[diesel(check_for_backend(Pg))]
+#[diesel(table_name=rooms)]
+pub struct RoomSettings {
+    pub name: String,
+    pub close_date: NaiveDateTime,
+    pub description: String,
+    pub room_url: String,
+    pub author_id: i64,
+    pub private: bool,
+    pub yaml_validation: bool,
+    pub allow_unsupported: bool,
+    pub yaml_limit_per_user: Option<i32>,
+    pub yaml_limit_bypass_list: Vec<i64>,
+    pub manifest: Json<Manifest>,
+    pub show_apworlds: bool,
+}
+
+impl RoomSettings {
+    pub fn default(index: &Index) -> Result<Self> {
+        Ok(Self {
+            name: "".to_string(),
+            close_date: chrono::Utc::now()
+                .naive_utc()
+                .with_second(0)
+                .context("Failed to create default datetime")?,
+            description: "".to_string(),
+            room_url: "".to_string(),
+            author_id: -1,
+            private: true,
+            yaml_validation: true,
+            allow_unsupported: false,
+            yaml_limit_per_user: None,
+            yaml_limit_bypass_list: vec![],
+            manifest: Json(Manifest::from_index_with_latest_versions(index)?),
+            show_apworlds: true,
+        })
+    }
+}
+
+impl Room {
+    pub fn is_closed(&self) -> bool {
+        self.settings.close_date < chrono::offset::Utc::now().naive_utc()
+    }
+}
+
+#[tracing::instrument(skip(conn))]
+pub async fn create_room<'a>(
+    new_room: &'a NewRoom<'a>,
+    conn: &mut AsyncPgConnection,
+) -> Result<Room> {
+    Ok(diesel::insert_into(rooms::table)
+        .values(new_room)
+        .returning(Room::as_returning())
+        .get_result(conn)
+        .await?)
+}
+
+#[tracing::instrument(skip(conn))]
+pub async fn update_room<'a>(
+    new_room: &'a NewRoom<'a>,
+    conn: &mut AsyncPgConnection,
+) -> Result<()> {
+    diesel::update(rooms::table)
+        .filter(rooms::id.eq(new_room.id))
+        .set(new_room)
+        .execute(conn)
+        .await?;
+
+    Ok(())
+}
+
+#[tracing::instrument(skip(conn))]
+pub async fn delete_room(room_id: &uuid::Uuid, conn: &mut AsyncPgConnection) -> Result<()> {
+    diesel::delete(rooms::table)
+        .filter(rooms::id.eq(room_id))
+        .execute(conn)
+        .await?;
+
+    Ok(())
+}
+
+#[tracing::instrument(skip(conn))]
+pub async fn get_room(uuid: uuid::Uuid, conn: &mut AsyncPgConnection) -> Result<Room> {
+    Ok(rooms::table
+        .find(uuid)
+        .select(Room::as_select())
+        .first::<Room>(conn)
+        .await?)
+}
+
+#[tracing::instrument(skip(conn))]
+pub async fn get_room_and_author(
+    uuid: uuid::Uuid,
+    conn: &mut AsyncPgConnection,
+) -> Result<(Room, String)> {
+    Ok(rooms::table
+        .find(uuid)
+        .inner_join(discord_users::table)
+        .select((Room::as_select(), discord_users::username))
+        .first(conn)
+        .await?)
+}
