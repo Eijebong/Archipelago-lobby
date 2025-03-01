8384f1c2d8b9b50ffd7e1a03887794c5bbb4301e
Bastien Orivel <eijebong@bananium.fr>
2025-03-01 13:00:11 +0100

    Fix FFXIV fishsanity
    Fixes #47

--- src/extractor/ffxiv.rs
+++ src/extractor/ffxiv.rs
@@ -1,6 +1,9 @@
-use super::{Extractor, FeatureExtractor, YamlFeature};
+use serde_yaml::Value;
+
+use super::{is_trueish, Extractor, FeatureExtractor, YamlFeature};
 use crate::error::Result;
 
+#[allow(clippy::upper_case_acronyms)]
 pub struct FFXIV;
 
 impl FeatureExtractor for FFXIV {
@@ -9,8 +12,24 @@ impl FeatureExtractor for FFXIV {
     }
 
     fn extract_features(&self, extractor: &mut Extractor) -> Result<()> {
-        extractor.register_feature(YamlFeature::FishSanity, "fishsanity")?;
+        extractor.register_feature_with_custom_truth(
+            YamlFeature::FishSanity,
+            "fishsanity",
+            fishsanity_trueish,
+        )?;
 
         Ok(())
     }
+}
+
+fn fishsanity_trueish(value: &Value) -> bool {
+    if is_trueish(value) {
+        return true;
+    }
+
+    if let Some(v) = value.as_str() {
+        return v.ends_with("_fish");
+    }
+
+    false
 }
