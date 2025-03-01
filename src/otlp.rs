c4ea17c492b01b099568b132581d56e12d6b946e
Bastien Orivel <eijebong@bananium.fr>
2025-03-01 13:52:19 +0100

    Update some deps

--- src/otlp.rs
+++ src/otlp.rs
@@ -2,14 +2,10 @@ use opentelemetry::{global, trace::TracerProvider, KeyValue};
 use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::{
     propagation::TraceContextPropagator,
-    runtime,
-    trace::{BatchConfig, RandomIdGenerator, Tracer},
+    trace::{RandomIdGenerator, SdkTracerProvider},
     Resource,
 };
-use opentelemetry_semantic_conventions::{
-    resource::{DEPLOYMENT_ENVIRONMENT, SERVICE_NAME},
-    SCHEMA_URL,
-};
+use opentelemetry_semantic_conventions::attribute::DEPLOYMENT_ENVIRONMENT_NAME;
 use rocket::{
     fairing::{Fairing, Info, Kind},
     Request, Response,
@@ -19,38 +15,31 @@ use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 fn resource() -> Resource {
-    Resource::from_schema_url(
-        [
-            KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
-            KeyValue::new(
-                DEPLOYMENT_ENVIRONMENT,
-                std::env::var("ROCKET_PROFILE").unwrap_or_else(|_| "dev".to_string()),
-            ),
-        ],
-        SCHEMA_URL,
-    )
+    Resource::builder()
+        .with_service_name(env!("CARGO_PKG_NAME"))
+        .with_attribute(KeyValue::new(
+            DEPLOYMENT_ENVIRONMENT_NAME,
+            std::env::var("ROCKET_PROFILE").unwrap_or_else(|_| "dev".to_string()),
+        ))
+        .build()
 }
 
-fn init_tracer(endpoint: &str) -> Tracer {
-    let provider = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_trace_config(
-            opentelemetry_sdk::trace::Config::default()
-                .with_id_generator(RandomIdGenerator::default())
-                .with_resource(resource()),
+fn init_tracer(endpoint: &str) -> SdkTracerProvider {
+    let provider = SdkTracerProvider::builder()
+        .with_id_generator(RandomIdGenerator::default())
+        .with_resource(resource())
+        .with_batch_exporter(
+            opentelemetry_otlp::SpanExporter::builder()
+                .with_tonic()
+                .with_endpoint(endpoint)
+                .build()
+                .unwrap(),
         )
-        .with_batch_config(BatchConfig::default())
-        .with_exporter(
-            opentelemetry_otlp::new_exporter()
-                .tonic()
-                .with_endpoint(endpoint),
-        )
-        .install_batch(runtime::Tokio)
-        .unwrap();
+        .build();
 
     global::set_tracer_provider(provider.clone());
     global::set_text_map_propagator(TraceContextPropagator::new());
-    provider.tracer("tracing-otel-subscriber")
+    provider
 }
 
 pub fn init_tracing_subscriber(endpoint: Option<String>) -> OtelGuard {
@@ -61,22 +50,30 @@ pub fn init_tracing_subscriber(endpoint: Option<String>) -> OtelGuard {
         .with(tracing_subscriber::fmt::layer())
         .with(sentry_tracing::layer());
 
-    if let Some(endpoint) = endpoint {
-        let tracer = init_tracer(&endpoint);
+    let provider = if let Some(endpoint) = endpoint {
+        let provider = init_tracer(&endpoint);
+        let tracer = provider.tracer("tracing-otel-subscriber");
         subscriber.with(OpenTelemetryLayer::new(tracer)).init();
+
+        Some(provider)
     } else {
         subscriber.init();
         tracing::warn!("No OTLP_ENDPOINT specified, not enabling opentelemetry");
+        None
     };
 
-    OtelGuard {}
+    OtelGuard { provider }
 }
 
-pub struct OtelGuard {}
+pub struct OtelGuard {
+    provider: Option<opentelemetry_sdk::trace::SdkTracerProvider>,
+}
 
 impl Drop for OtelGuard {
     fn drop(&mut self) {
-        opentelemetry::global::shutdown_tracer_provider();
+        if let Some(tracer) = self.provider.take() {
+            let _ = tracer.shutdown();
+        }
     }
 }
 
