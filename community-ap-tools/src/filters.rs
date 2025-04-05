16dba587db9135ec30f1bf7b152ed24f14d542f9
Bastien Orivel <eijebong@bananium.fr>
2025-04-05 18:53:46 +0200

    Add last activity

--- community-ap-tools/src/filters.rs
+++ community-ap-tools/src/filters.rs
@@ -1,3 +1,5 @@
+use humantime::format_duration;
+
 use crate::guards::{SlotInfo, SlotStatus};
 
 pub fn slot_status(slot: &SlotInfo) -> askama::Result<&str> {
@@ -34,4 +36,13 @@ pub fn slot_status(slot: &SlotInfo) -> askama::Result<&str> {
 
         return Ok("green");
     }
+}
+
+pub fn last_active(slot: &SlotInfo) -> askama::Result<String> {
+    let Some(activity) = slot.last_activity else {
+        return Ok("Never".to_string());
+    };
+
+    let d = std::time::Duration::from_secs(activity as u64);
+    Ok(format_duration(d).to_string())
 }
