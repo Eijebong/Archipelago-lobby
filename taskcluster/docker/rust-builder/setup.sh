00efca80f874a0801fb23837f5ca7f3f1e7f31b6
Bastien Orivel <eijebong@bananium.fr>
2025-06-17 22:50:04 +0200

    Reduce the size of the rust builder image

---
+++ taskcluster/docker/rust-builder/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+printf "deb http://httpredir.debian.org/debian bookworm-backports main" > /etc/apt/sources.list.d/backports.list
+apt update && apt install -y libpq-dev valkey
+apt autoremove -y
+rm -rf /var/lib/apt/lists/*
+
+rustup component add clippy rustfmt
+
+cargo install cargo-watch
+
+rm -Rf /usr/local/cargo
+# Add worker user
+mkdir -p /builds
+useradd -d /builds/worker -s /bin/bash -m worker
+mkdir -p /builds/worker/artifacts
+chown -R worker:worker /builds/worker
