3b4169eacbc34bee583d6f21f81ca4d8690e2081
Bastien Orivel <eijebong@bananium.fr>
2025-07-28 19:20:44 +0200

    Add indices on rooms to make metrics less time consuming

---
+++ lobby/migrations/2025-07-28-171959_add_indices_room_times/down.sql
@@ -0,0 +1,2 @@
+DROP INDEX idx_rooms_close_date;
+DROP INDEX idx_rooms_updated_at;
---
+++ lobby/migrations/2025-07-28-171959_add_indices_room_times/up.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_rooms_close_date
+  ON rooms(close_date);
+
+CREATE INDEX idx_rooms_updated_at
+  ON rooms(updated_at);
