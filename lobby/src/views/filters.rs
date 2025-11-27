Eijebong/Archipelago-lobby

commit 0eb134240b90e73d3889ff7cb3740e1cf0e774c7

Use ammonia to cleanup the html from markdown
This is mostly to add rel attributes to links because comrak doesn't by
default

lobby/src/views/filters.rs
@@ -75,8 +75,8 @@ where
     defaults.extension.autolink = true;
     defaults.render.escape = true;
 
-    let s = markdown_to_html(text, &defaults);
-    Ok(s)
+    let html = markdown_to_html(text, &defaults);
+    Ok(ammonia::clean(&html))
 }
 
 fn tag_to_icon(tag: &WorldTag) -> &str {

lobby/Cargo.toml
@@ -47,6 +47,7 @@ tracing-subscriber = "0.3.19"
 opentelemetry-semantic-conventions = { version = "0.30.0", features = ["semconv_experimental"] }
 counter = "0.7.0"
 comrak = { version = "0.41", default-features = false }
+ammonia = "4"
 wq = { path = "../wq" }
 sentry = "0.38.1"
 sentry-tracing = "0.38.1"

commit 38634857a7b849e51ec78a561109e7f862470e9b

Allow data URL schemes
This fixes a regression from 0eb134240b90e73d3889ff7cb3740e1cf0e774c7
where I added ammonia to cleanup links but in the process broke images
using the data scheme.

Fixes #105

lobby/src/views/filters.rs
@@ -76,7 +76,11 @@ where
     defaults.render.escape = true;
 
     let html = markdown_to_html(text, &defaults);
-    Ok(ammonia::clean(&html))
+    let sanitized = ammonia::Builder::default()
+        .add_url_schemes(&["data"])
+        .clean(&html)
+        .to_string();
+    Ok(sanitized)
 }
 
 fn tag_to_icon(tag: &WorldTag) -> &str {